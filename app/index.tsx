c190753401b75a28324f538051734095cbd18c38
SHIBIN K A <shibin.k.a6235@gmail.com>
2025-01-06 20:25:27 +0530

    style: StatusBar color change

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,40 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+const image = require("../assets/images/react-logo.png")
 
 export default function Index() {
   return (
     <>
-      <Stack.Screen options={{ headerShown: false, title: 'Home', headerStyle: { backgroundColor: "red" } }} />
+      <Stack.Screen options={{ headerShown: true, title: 'Home', headerTransparent: true, headerTitleStyle: { color: "white" }, headerStyle: { backgroundColor: "#000000df" } }} />
+      <StatusBar style="dark" backgroundColor="#0d1117" />
       <View
         style={{
           flex: 1,
-          justifyContent: "center",
+          justifyContent: "flex-start",
           alignItems: "center",
-          backgroundColor: "green"
+          backgroundColor: "#0d1117",
+          paddingTop: 50
         }}
       >
-        <Text style={{ color: "white", fontSize: 20, fontWeight: 700 }}>Hello World</Text>
-        <StatusBar style="dark" backgroundColor="blue" />
+
+        <View
+          style={{
+            width: 200,
+            height: 200,
+            backgroundColor: "gold",
+            marginTop: 60,
+            borderRadius: 10
+          }}
+        >
+          <Text style={{
+            color: "white", fontSize: 20, fontWeight: 700
+          }}>Hello World</Text>
+        </View>
+        <View style={{ display: "flex", alignItems: "center", justifyContent: "center", width: 200, height: 200, backgroundColor: "dodgerblue", borderRadius: 10, marginTop: 10 }}>
+          <Image source={image} style={{ width: 150, height: 150, objectFit: "cover" }} />
+        </View>
       </View>
     </>
 
