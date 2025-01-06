c596ec7b424d44ae18e46d5d76b029476e59907a
SHIBIN K A <shibin.k.a6235@gmail.com>
2025-01-06 23:00:46 +0530

    chore: nativewind lib added

--- app/index.tsx
+++ app/index.tsx
@@ -3,34 +3,20 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Image, Text, View } from "react-native";
 const image = require("../assets/images/react-logo.png")
+import "../global.css";
 
 export default function Index() {
   return (
     <>
       <Stack.Screen options={{ headerShown: true, title: 'Home', headerTransparent: true, headerTitleStyle: { color: "white" }, headerStyle: { backgroundColor: "#000000df" } }} />
       <StatusBar style="dark" backgroundColor="#0d1117" />
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "flex-start",
-          alignItems: "center",
-          backgroundColor: "#0d1117",
-          paddingTop: 50
-        }}
-      >
-
-        <View
-          style={{
-            width: 200,
-            height: 200,
-            backgroundColor: "gold",
-            marginTop: 60,
-            borderRadius: 10
-          }}
+      <View className="flex-1 justify-start items-center bg-[#0d1117] pt-14 p-4">
+        <View className="w-full h-52 bg-yellow-500 mt-16 rounded-lg"
         >
           <Text style={{
             color: "white", fontSize: 20, fontWeight: 700
           }}>Hello World</Text>
+          <Image source={image} className="w-10 h-10" />
         </View>
         <View style={{ display: "flex", alignItems: "center", justifyContent: "center", width: 200, height: 200, backgroundColor: "dodgerblue", borderRadius: 10, marginTop: 10 }}>
           <Image source={image} style={{ width: 150, height: 150, objectFit: "cover" }} />
